4adfb588e931ece09667484bb56edcee8e008dd3
Maxym <perebyinis.maksym@lll.kpi.ua>
2024-09-21 20:30:37 +0300

    xaml markup for main window

--- App.xaml.cs
+++ App.xaml.cs
@@ -16,20 +16,10 @@ using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace СaesarCipher
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     public partial class App : Application
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             this.InitializeComponent();
@@ -42,6 +32,7 @@ namespace СaesarCipher
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
             m_window = new MainWindow();
+            
             m_window.Activate();
         }
 
